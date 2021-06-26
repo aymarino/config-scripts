9a6cd255a11737a4c9741902ad9eba0b5386326e
Andrew Marino <andrewymarino@gmail.com>
2021-06-25 17:06:55 -0700

    Add to ubuntu setup script

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,5 +1,6 @@
 
 function exists() {
+  echo "Checking if command '$1' exists..."
   command -v "$1" &> /dev/null
 }
 
@@ -10,22 +11,47 @@ function ensure_exists() {
   fi
 }
 
+function apt_installed() {
+  echo "Checking if package '$1' is installed..."
+  dpkg -s $1 &> /dev/null
+}
+
+function apt_install() {
+  if ! apt_installed $1 ; then
+    echo "Installing package '$1' through apt..."
+    sudo apt-get install $1
+  fi
+}
+
 ensure_exists git
 sh ./git-setup.sh
 
-# Python: python3, pip, pipenv, pyenv (?)
+# Essentials
+apt_install build-essential
 
+# TODO: Python: python3, pip, pipenv, pyenv (?)
+
+# Utilities
+apt_install ripgrep
+
+# AWS CLI 2
 if ! exists aws ; then
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
   sudo ./aws/install
   ensure_exists aws
   rm awscliv2.zip
-  # rm -rf aws -- confirm
+  # rm -rf aws # confirm
 fi
 
+# Elastic Beanstalk CLI
 if ! exists eb ; then
   pip install awsebcli
 fi
 
 # Jekyll
+apt_install ruby-full
+apt_install zlib1g-dev
+if ! exists jekyll ; then
+  sudo gem install jekyll bundler
+fi
