b058c797e1cc47b6f27bebf36e1cd159f516b03f
Andrew Marino <andrewymarino@gmail.com>
2021-12-16 12:11:51 -0800

    Make colors dark mode/light mode agnostic

--- git-setup.sh
+++ git-setup.sh
@@ -11,4 +11,9 @@ fi
 git config --global core.editor "vim"
 
 # Aliases
-git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%aD)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:\
+'%C(bold blue)%h%C(reset) - \
+%C(bold green)(%aD)%C(reset) \
+%s \
+%C(dim)- %an%C(reset)\
+%C(bold yellow)%d%C(reset)' --all"
