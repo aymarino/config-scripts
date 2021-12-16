80855d81adc207f58166d45260bd9dc0e6175f40
Andrew Marino <andrewymarino@gmail.com>
2021-12-16 12:00:57 -0800

    Add git alias for pretty git log

--- git-setup.sh
+++ git-setup.sh
@@ -1,8 +1,14 @@
 # Git setup
 
+# Author details
+git config --global user.name "Andrew Marino"
 if ! git config --get user.email > /dev/null ; then
   read -p 'Enter git config user.email: ' email
   git config --global user.email "$email"
 fi
-git config --global user.name "Andrew Marino"
+
+# Editor
 git config --global core.editor "vim"
+
+# Aliases
+git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%aD)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
