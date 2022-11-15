17ebee3b758de6979db5b82612680a88cce511a6
Andrew Marino <andrewymarino@gmail.com>
2022-11-14 17:02:42 -0800

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -39,12 +39,17 @@ if ! exists brew ; then
   echo "'brew' not found; installing ..."
   # From official site: brew.sh
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  ensure_exists brew
 fi
 
 # Utilities
 brew_install_login_app mos # reverse scroll wheel direction
 brew_install_login_app rectangle # gives Windows-style max/half screen shortcuts
 brew_install_login_app maccy # Gives clipboard history
+brew_install_login_app notunes
+brew_install visual-studio-code
 brew_install jq
 if brew_install fzf ; then
   $(brew --prefix)/opt/fzf/install # Installs key bindings and `**` command completion
@@ -60,6 +65,8 @@ if ! exists python3 ; then
 fi
 ensure_exists pip3
 
+pip3 install pyyaml # Used for `update-vscode-settings` util
+
 # 'ssh-config' used by start-ec2-dev script
 if ! exists ssh-config ; then
   pip3 install ssh-config
@@ -73,7 +80,6 @@ fi
 
 # Add scripts to bin
 add_script_to_bin start-ec2-dev
-add_script_to_bin_as auth-mac auth
 add_script_to_bin remote-workspace
 
 # Add ~/.scripts-bin to PATH
